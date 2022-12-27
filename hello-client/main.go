c27093c14a5c4007ebcdd03977da575086ab8e4c
huzejun <huzejun1990>
2022-12-28 03:22:24 +0800

    tsl + token认证（tsl可能存在问题 openssl.cfg/cnf在安装的文件中不一定有） 2022-12-28 03:21:55

--- hello-client/main.go
+++ hello-client/main.go
@@ -9,9 +9,36 @@ import (
 	"log"
 )
 
+type PerRPCCredentials interface {
+	GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error)
+	RequireTransportSecurity() bool
+}
+
+type ClientTokenAuth struct {
+}
+
+func (c ClientTokenAuth) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appId":  "dream",
+		"appKey": "123123",
+	}, nil
+}
+
+func (c ClientTokenAuth) RequireTransportSecurity() bool {
+	return false
+
+}
+
 func main() {
+	//creds,_ := credentials.NewClientTLSFromFile("F:\\environment\\GoWorks\\src\\huzejun_go\\dream-grpc-study\\key\\privkey.pem","*.dream.com")
+
 	// 连接到server端，此处禁用安全传输，没有加密和验证
-	conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithPerRPCCredentials(new(ClientTokenAuth)))
+
+	conn, err := grpc.Dial("127.0.0.1:9090", opts...)
+	//conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
--- hello-server/main.go
+++ hello-server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	pb "huzejun_go/dream-grpc-study/hello-server/proto"
 	"net"
 )
@@ -13,16 +16,43 @@ type server struct {
 	pb.UnimplementedSayHelloServer
 }
 
+// 业务
 func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+
+	//获取元数据信息
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("未传输token")
+	}
+	var appId string
+	var appKey string
+	if v, ok := md["appid"]; ok {
+		appId = v[0]
+	}
+	if v, ok := md["appkey"]; ok {
+		appKey = v[0]
+	}
+	if appId != "dream" || appKey != "123123" {
+		return nil, errors.New("token 不正确！")
+	}
+
 	fmt.Printf("hello" + req.RequestName)
+
 	return &pb.HelloResponse{ResponseMsg: "hello" + req.RequestName}, nil
 }
 
 func main() {
+	//===================
+	//TSL认证
+	//========
+	// 两个参数分别是 cretFile,keyFile
+	//自签名证书文件加和私钥文件
+	//creds, _ := credentials.NewServerTLSFromFile("F:\\environment\\GoWorks\\src\\huzejun_go\\dream-grpc-study\\key\\privkey.pem","F:\\environment\\GoWorks\\src\\huzejun_go\\dream-grpc-study\\key\\test.key")
 	// 开启端口
 	listen, _ := net.Listen("tcp", ":9090")
 	// 创建 grpc 服务
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+	//grpcServer := grpc.NewServer(grpc.Creds(creds))
 	// 在grpc服务端中去注册我们自己编写的服务
 	pb.RegisterSayHelloServer(grpcServer, &server{})
 
